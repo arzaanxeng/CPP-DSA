f3eb829bb48193c463cd0d20caa15b591e0ed57c
Arzaan <arzaansyed2@gmail.com>
2026-05-18 15:06:24 +0530

    Profit and Loss

---
+++ BASICS-CPP/code_3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int a , b;
+    cout<<"Enter the COST PRICE : $";
+    cin>>a;
+    cout<<"Enter the SELLING PRICE : $";
+    cin>>b;
+
+    if(a>b){
+        cout<<"Sorry! You have made a loss of $"<< a - b <<endl;
+    }
+    else if(b>a){
+        cout<<"Yeah! You have made a profit of $"<< b-a <<endl;
+    }
+    else {
+        cout<<"Neither profit nor gain";
+    }
+
+}
