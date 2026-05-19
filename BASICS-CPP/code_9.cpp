b1f49a991790cd0c501a8a56786dc1c30ff27448
Arzaan <arzaansyed2@gmail.com>
2026-05-19 17:28:09 +0530

    Prime numbers

---
+++ BASICS-CPP/code_9.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    int n;
+    cout << "Enter how many prime numbers you would like to print: ";
+    cin >> n;
+    if (n <= 0) 
+    cout<<"Please enter a natural number!";
+
+    cout << "The first " << n << " prime numbers are: \n";
+    int count = 0; 
+    int i = 2;    
+    while(count < n){
+        bool flag = true; // Assuming the number is prime
+        for( int j = 2 ; j < i ; j++){
+            if(i%j == 0){
+                flag = false;
+                break;
+            }
+        }
+        if(flag == true){
+            cout<<i<<' ';
+            count++;
+        }
+
+        i++;
+    }
+    return 0;
+}
+
+
+  
