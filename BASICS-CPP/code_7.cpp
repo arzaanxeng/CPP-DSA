66fe0ded85a3e9cb0b03495bbdb0732122ac4042
Arzaan <arzaansyed2@gmail.com>
2026-05-19 15:53:11 +0530

    SUM AND REVERSE OF A NUMBER

--- BASICS-CPP/code_7.cpp
+++ BASICS-CPP/code_7.cpp
@@ -1,1 +1,28 @@
-// SUM OF THE DIGITS OF A NUMBER
+
+#include<iostream>
+using namespace std;
+int main(void){
+int n ;
+   // SUM OF THE DIGITS OF A NUMBER
+   cout<<"=== SUM OF THE DIGITS OF A NUMBER ==="<<endl;
+   cout<<"Enter the required number: ";
+   cin>>n;
+   int sum = 0;
+   int n_temp = n;
+   while(n > 0){
+   sum = sum + n%10;
+   n = n/10;
+   }
+   cout<<"The sum of the digits of the number is :"<<sum<<endl;
+
+   // REVERSING THE NUMBER
+   int reversed_number = 0 , r;
+   cout<<"\n=== REVERSE OF THE NUMBER ==="<<endl;
+   while(n_temp >0){
+    r = n_temp%10;
+    reversed_number = (reversed_number*10) + r;
+    n_temp = n_temp/10;
+   }
+   cout<<"The reversed number is :"<<reversed_number;
+
+} 
