b9b9d945c9c11af105875bd7b15c24746857668e
Arzaan <arzaansyed2@gmail.com>
2026-06-04 20:14:50 +0530

    Sub arrays

---
+++ ARRAYS-CPP/code_36.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+// PRINTING THE REQUIRED SUB ARRAY
+int main(void){
+  int arr[] = {1,7,5,9,21,65,98,78,65,45,23};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  int l , r;
+  cout<<"Enter the value of left index :";
+  cin>>l;
+  cout<<"Enter the value of right index :";
+  cin>>r;
+   
+  cout<<"The subarrays are : \n\n";
+  for(int i = l ; i <= r ; i++){
+     for(int j = i ; j <= r ; j++){
+        for( int k = i ; k <= j ; k++) {
+            cout<<arr[k]<<" ";
+     }
+     cout<<endl;
+  }
+}
+
+}
