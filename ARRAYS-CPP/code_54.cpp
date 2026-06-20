6956c119d5e44c1a7c4129627b8c87c98208352a
Arzaan <25bee110@nith.ac.in>
2026-06-20 18:19:48 +0530

    Unique elements containg arr : Optimal Soln

--- ARRAYS-CPP/code_54.cpp
+++ ARRAYS-CPP/code_54.cpp
@@ -1,6 +1,6 @@
 // Retreiving the array with unique elements only 
 #include<iostream>
-#include<vector>
+#include<set>
 using namespace std;
 int main(void){
     int n;
@@ -11,22 +11,14 @@ int main(void){
     cout<<"Enter the value of element number "<<(i+1)<<" :";
     cin>>arr[i];
     }
-    vector<int>v;
-    for(int i = 0 ; i<n ; i++){
-        bool duplicate = false;
-        for(int j = 0 ; j<n ; j++){
-            if((arr[i] == arr[j]) && (i!=j)) {
-                duplicate = true;
-                break;
-            }
-        }
-        if(!duplicate) v.push_back(arr[i]);
+    set<int>s;
+    for(int i =0 ; i<n ; i++){
+        s.insert(arr[i]);
     }
-
-    for(auto it = v.begin() ; it != v.end() ; it++){
+    for(auto it = s.begin() ; it != s.end() ; it++){
         cout<<*it<<" ";
     }
-
+    
     return 0;
 }
 
