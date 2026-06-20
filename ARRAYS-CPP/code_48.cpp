739586e4c5e478866dc782f44b567d42b01357c3
Arzaan <25bee110@nith.ac.in>
2026-06-20 16:12:37 +0530

    Google Problem TC : O(N) & SC : O(1)

---
+++ ARRAYS-CPP/code_48.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int n;
+    cout<<"Enter the number of elements in the array :";
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i<n ; i++){ 
+    cout<<"Enter the value of element number "<<(i+1)<<" :";
+    cin>>arr[i];
+    }
+
+
+
+
+    /* BRUTE FORCE APPROACH
+    int ans =0;
+    for(int i = 0 ; i<n ; i++){
+        int sum = 0;
+        for(int j = i ; j< n ; j++){
+            for(int k = i ; k <= j ; k++){
+                sum += arr[k];
+            }
+        }
+        ans += sum;
+    }
+
+    int prefix_arr[n] , sum = 0;
+    for(int i =0 ; i<n ; i++){
+        sum += arr[i];
+        prefix_arr[i] = sum;
+    }
+    
+   int sum2 =0;
+   for(int i =0 ; i<n ; i++){
+    for(int k = i ; k<n ; k++){
+        if(i==0) sum2+= prefix_arr[k];
+        else sum2 += (prefix_arr[k] - prefix_arr[i-1]);
+    }
+   }
+   cout<<sum2;
+   */
+int sum = 0;
+   for(int i = 0 ; i < n ; i++){
+    sum += arr[i]*(i+1)*(n-i);
+   }
+   cout<<"sum is : "<<sum;
+ }
