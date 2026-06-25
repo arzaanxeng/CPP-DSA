dfd9c8ad3a346abd3f46f44f51d27476d06b9274
Arzaan <25bee110@nith.ac.in>
2026-06-25 10:49:13 +0530

    Longest Subarray with sum < k

---
+++ ARRAYS-CPP/code_70.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void find_longest_subarray(vector<int>& v , int k){
+    int j = 0 , max_length = 0 , sum = 0;
+    int n = v.size();
+    for(int i = 0 ; i < n ; i++){
+        sum +=v[i];
+        while(sum >= k){
+            sum-=v[j];
+            j++;
+        }
+        max_length = max(max_length , i-j+1);
+    }
+    cout<<"The maximum legth of subarray with sum just below "<<k<<" is :"<<max_length;
+}
+
+
+int main() {
+    int n, k;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> v[i];
+    }
+    cout << "Enter the upper bound of the subarray sum : ";
+    cin >> k;
+
+    find_longest_subarray(v, k);
+    return 0;
+}
