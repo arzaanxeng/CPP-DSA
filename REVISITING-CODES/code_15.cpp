4febfcddfe8e915b43286a49f753db5eb5c48451
Arzaan <25bee110@nith.ac.in>
2026-07-24 13:36:54 +0530

    Duplicates in an array

---
+++ REVISITING-CODES/code_15.cpp
@@ -0,0 +1,31 @@
+// Finding wether the array contains a duplicate element
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+void is_duplicate(vector<int> &v){
+    unordered_set<int>s;
+    for(int i : v){
+        if(s.find(v[i]) != s.end()){
+            cout<<"Yes , the array has duplicates!";
+            return;
+        }
+        else{
+            s.insert(v[i]);
+        }
+    }
+    cout<<"No , the array does not contain duplicates!";
+}
+
+int main(void){
+    int n;
+    cout<<"Enter the number of elements in the array : ";
+    cin>>n;
+    vector<int> v(n);
+    for(int i = 0 ; i < n ; i++){
+        cout<<"Enter the value of "<<(i+1)<<" element : ";
+        cin>>v[i];
+    }
+    is_duplicate(v);
+}
