8c8ebc8320e7762db6530af28a5a789b858a50a7
Arzaan <25bee110@nith.ac.in>
2026-07-26 09:44:06 +0530

    The Last True Index - II

---
+++ INTERMEDIATE-CPP/code_14.cpp
@@ -0,0 +1,19 @@
+// Finding the last true
+// EXAMPLE : "TTTTTTTTTTTTTFFFFFFF"
+#include<iostream>
+#include<string>
+using namespace std;
+int main(void){
+    string s = "TTTTTTTTTTTTTFFFFFFF";
+    int l = 0 , r = s.size() - 1 , ans = -1 ;
+    while( l <= r){
+        int m = l + (r-l)/2;
+        if(s[m] == 'T'){
+            ans = m;
+            l = m+1;
+        }
+        else r = m-1;
+    }
+    if (ans == -1 ) cout << "No 'T' found in the string\n";
+    else cout << "The last true value is at index: " << l << "\n";
+}
