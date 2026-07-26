f8b0e5500d49041d221dabf9ac06265c0300df08
Arzaan <25bee110@nith.ac.in>
2026-07-26 09:17:12 +0530

    Leading True Value Index

---
+++ INTERMEDIATE-CPP/code_12.cpp
@@ -0,0 +1,19 @@
+// Finding the first true
+// EXAMPLE : "FFFFFFFTTTTTTTTTTTTT"
+#include<iostream>
+#include<string>
+using namespace std;
+int main(void){
+    string s = "FFFFFFFFFTTTTTTTTTTTTT";
+    int l = 0 , r = s.size() - 1 , ans = -1 ;
+    while( l <= r){
+        int m = l + (r-l)/2;
+        if(s[m] == 'T'){
+            ans = m;
+            r = m-1;
+        }
+        else l = m+1;
+    }
+    if (ans == -1 ) cout << "No 'T' found in the string\n";
+    else cout << "The leading true value is at index: " << l << "\n";
+}
