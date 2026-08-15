a752d8df648a62250d88f2477d9935b6d49c990c
Arzaan <25bee110@nith.ac.in>
2026-08-15 15:17:45 +0530

    27. Remove Element

---
+++ BASICS-CPP/code_64.cpp
@@ -0,0 +1,20 @@
+// 27. Remove Element 
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int idx  = 0;
+        for( int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] != val){
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx;
+    }
+};
