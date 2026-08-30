697296eea53d2fd1545e54dcd1bcc1886114d2f0
Arzaan <25bee110@nith.ac.in>
2026-08-30 09:45:02 +0530

    Reversing a number - Leetcode Medium

---
+++ BASICS-CPP/code_70.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long ans = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+            ans = ans * 10 + rem;
+            x /= 10;
+        }
+
+        if (ans > INT_MAX || ans < INT_MIN)
+            return 0;
+
+        return (int)ans;
+    }
+};
