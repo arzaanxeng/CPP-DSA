28fcd8cee4b87bf2987d412f3d69fa5011edcdb5
Arzaan <25bee110@nith.ac.in>
2026-09-01 17:02:18 +0530

    Sorted Array Check using Recursion

---
+++ BASICS-CPP/code_81.cpp
@@ -0,0 +1,26 @@
+// Is Sorted
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSorted(const int nums[], int n){
+    if( n == 0 ) return true;
+    bool smallAns = isSorted(nums,n-1);
+    if( smallAns == false ) return false;
+    return nums[n-1] >= nums[n-2];
+}
+
+int main(void){
+    int n, x;
+    cout<<"Enter the number of elements in the array : ";
+    cin >> n;
+    int arr[n];
+    for( int i = 0 ; i < n ; i++){ 
+    cout<<"Enter the value of element number "<<(i+1)<<" : "; 
+    cin>>arr[i]; 
+    } 
+    bool ans = isSorted(arr,n);
+    if(ans)cout<<"Yes";
+    else cout<<"No";
+}
