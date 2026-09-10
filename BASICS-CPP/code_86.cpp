236525383f569b956b2c8a886dce5e2e264d7e1a
Arzaan <25bee110@nith.ac.in>
2026-09-10 12:32:05 +0530

    Finding Needle In Haystack

---
+++ BASICS-CPP/code_86.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int ans = -1;
+        ans =  haystack.find(needle);
+        return ans;
+    }
+};
